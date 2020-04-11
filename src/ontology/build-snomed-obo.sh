e3a0475d8898ee2cf7c53ef8fd4a55bca33504b4
Bill Duncan <wdduncan@gmail.com>
2020-04-11 09:44:23 -0400

    add axioms-boomer.owl target to makefile

--- src/ontology/build-snomed-obo.sh
+++ src/ontology/build-snomed-obo.sh
@@ -5,10 +5,11 @@
 #sed -n '1,3 p' snomed-mixed.obo > header.obo # print out lines 1-3
 
 ## easier to just build a new header
-echo $'format-version: 1.2\nontology: snomed' > header.obo
+echo $'format-version: 1.2' > header.obo
+echo $'ontology: snomed' >> header.obo
 
 ## output everyting from line 5 on
-tail -n +5 snomed-mixed.obo > body.obo
+tail -n +5 snomed.obo > body.obo
 
 ## merger header and body
 cat header.obo body.obo > tmp.obo
